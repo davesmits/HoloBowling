879d4bd5646d639b895119c6d41b1eb31ab589e1
Guido Kersten <guido.kersten@live.nl>
2017-03-15 21:18:21 +0100

    Adds throwing bomb

--- Unity/HoloBowling/Assets/HoloBowling/Scripts/Explosion.cs
+++ Unity/HoloBowling/Assets/HoloBowling/Scripts/Explosion.cs
@@ -6,11 +6,19 @@ public class Explosion : MonoBehaviour
 {
     private ParticleSystem _particleSystem;
     private AudioSource _audioSource;
+    private Rigidbody _rigidBody;
 
     private void Start()
     {
         _particleSystem = GetComponent<ParticleSystem>();
         _audioSource = GetComponent<AudioSource>();
+        _rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        //_rigidBody.AddForce(Vector3.up * 7, ForceMode.Acceleration);
     }
 
     public void Explode()
