108883656a003a500a327c499f9df41e59727206
Guido Kersten <guido.kersten@live.nl>
2017-03-15 20:12:18 +0100

    Adds Models

---
+++ Unity/HoloBowling/Assets/Barrels.cs
@@ -0,0 +1,24 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Barrels : MonoBehaviour, IInputClickHandler {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var rigidbody = gameObject.AddComponent<Rigidbody>();
+        rigidbody.useGravity = true;
+    }
+}
