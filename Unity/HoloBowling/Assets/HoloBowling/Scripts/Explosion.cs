285a72e7ec7b3fbf81a60dedddeb95195624894e
Dave Smits <dave@familie-smits.com>
2017-03-15 20:28:14 +0100

    behavior to trigger explosion when bomb collides

--- Unity/HoloBowling/Assets/HoloBowling/Scripts/Explosion.cs
+++ Unity/HoloBowling/Assets/HoloBowling/Scripts/Explosion.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Explosion : MonoBehaviour {
+public class Explosion : MonoBehaviour
+{
+    private ParticleSystem _particleSystem;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     public void Explode()
     {
-        var exp = GetComponent<ParticleSystem>();
-        exp.Play();
-        Destroy(gameObject, exp.main.duration);
+        _particleSystem.Play();
+        _audioSource.Play();
+        Destroy(gameObject, _particleSystem.main.duration);
     }
 
 
     // Grenade explodes on impact.
     private void OnCollisionEnter(Collision coll)
     {
+        for (int index = 0; index < this.transform.childCount; index++)
+        {
+            var gameObject = this.transform.GetChild(index).gameObject;
+            gameObject.SetActive(false);
+        }
         Explode();
     }
 
